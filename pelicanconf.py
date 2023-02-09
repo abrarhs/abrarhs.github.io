1bce34cb056a77e417b78cbf7daf04dc066c4339
Abrar Hussain <77550655+abrarhs@users.noreply.github.com>
2023-02-08 17:59:08 -0700

    lab done

--- pelicanconf.py
+++ pelicanconf.py
@@ -10,7 +10,7 @@ DEFAULT_LANG = 'en'
 
 # Feed generation is usually not desired when developing
 FEED_ALL_ATOM = None
-#CATEGORY_FEED_ATOM = None
+CATEGORY_FEED_ATOM = None
 TRANSLATION_FEED_ATOM = None
 AUTHOR_FEED_ATOM = None
 AUTHOR_FEED_RSS = None
